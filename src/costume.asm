f4613e2b651a25c75ddc27582a6f3f036048062f
Jocelyn Beedie <jellonator00@gmail.com>
2024-04-26 10:19:59 -0500

    Sped up costume routine... at a cost

--- src/costume.asm
+++ src/costume.asm
@@ -3,9 +3,18 @@
 .BANK $01 SLOT "ROM"
 .SECTION "Player Costume" FREE
 
-; .STRUCT customedef_t
-
-; .ENDSTRUCT
+.MACRO .put_sprite ARGS spriteaddr, ntiles, spriteoffs, bufferoffs
+    php
+    phb
+    .ChangeDataBank bankbyte(spriteaddr)
+    ldy #loword(spriteaddr) + (spriteoffs * 48 * 4)
+    ldx #loword(playerSpriteBuffer) + (bufferoffs * 32 * 4)
+    lda #ntiles * 4
+    jsl Costume.blit_tiles_with_interlaced_mask
+    sep #$20
+    plb
+    plp
+.ENDM
 
 Costume.player_reset:
     ; copy data from `spritedata.isaac_head` to `playerSpriteBuffer`
@@ -26,56 +35,33 @@ Costume.player_recalculate:
     ; POLYPHEMUS
     lda.l playerData.playerItemStackNumber + ITEMID_POLYPHEMUS
     beq +
-        .ChangeDataBank bankbyte(spritedata.costume_polyphemus)
-        ldy #loword(spritedata.costume_polyphemus)
-        ldx #loword(playerSpriteBuffer)
-        lda #8 * 4
-        jsl Costume.copy_over
-        sep #$20
+        .put_sprite spritedata.costume_polyphemus, 8, 0, 0
+        .put_sprite spritedata.costume_polyphemus, 8, 0, 8
     +:
     lda.l playerData.playerItemStackNumber + ITEMID_WIRE_COAT_HANGER
     beq +
-        .ChangeDataBank bankbyte(spritedata.costume_wire_coat_hanger)
-        ldy #loword(spritedata.costume_wire_coat_hanger)
-        ldx #loword(playerSpriteBuffer)
-        lda #4 * 4
-        jsl Costume.copy_over
-        sep #$20
-        ldy #loword(spritedata.costume_wire_coat_hanger)
-        lda #4 * 4
-        jsl Costume.copy_over
-        sep #$20
-        ldy #loword(spritedata.costume_wire_coat_hanger)
-        lda #4 * 4
-        jsl Costume.copy_over
-        sep #$20
-        ldy #loword(spritedata.costume_wire_coat_hanger)
-        lda #4 * 4
-        jsl Costume.copy_over
-        sep #$20
+        .put_sprite spritedata.costume_wire_coat_hanger, 4, 0, 0
+        .put_sprite spritedata.costume_wire_coat_hanger, 4, 0, 4
+        .put_sprite spritedata.costume_wire_coat_hanger, 4, 0, 8
+        .put_sprite spritedata.costume_wire_coat_hanger, 4, 0, 12
     +:
     ; end
     plb
     rtl
 
-; Copy spritedata from DB,Y into $7E0000,X
-; Copy A tiles
-; Assumes 16b XY
-Costume.copy_over:
-    .INDEX 16
+; Blit A tiles from DB,Y into $7E0000,X
+Costume.blit_tiles_no_mask:
     sta.b $00
+    rep #$30
 @loop:
     ; note: we have 4bpp, with four bitplanes
     ; 0,Y: abcdefgh 1,Y: abcdefgh ... 16,Y: abcdefgh, 17,Y: abcdefgh
     ; for each incoming pixel aaaa,bbbb:
     ; write out bbbb if bbbb != 0, aaaa otherwise
-
-    rep #$20
     .REPT 8
         ; DETERMINE MASK bits in $04,$05,$06,$07 (maps to 0,1,16,17)
         ; set up four pixels at a time
         ; a-c-e-g- a-c-e-g-
-        ; lda #$FFFF
         stz.b $04
         lda.w $0000,Y
         and #$5555
@@ -130,14 +116,56 @@ Costume.copy_over:
         iny
     .ENDR
     ; while (--count)
-    ; rep #$20
     txa
     clc
     adc #16
     tax
     tya
     clc
+    adc #32
+    tay
+    dec.b $00
+    bnel @loop
+    ; end
+    rtl
+
+; Copy A tiles from DB,Y into $7E0000,X
+; Tiles blitted using this routine must include an interlaced mask.
+; The mask indicates which values in the copied are opaque.
+; Character data will be stored in the following order:
+;   16B: bitplanes 1 and 2
+;   16B: bitplanes 3 and 4
+;   16B: mask - each bit corresponds to a bit in each bitplane
+Costume.blit_tiles_with_interlaced_mask:
+    sta.b $00
+    rep #$30
+@loop:
+    .REPT 8
+        ; WRITE COLOR
+        lda.l $7F0000,X
+        eor.w $0000,Y
+        and.w $0020,Y
+        eor.l $7F0000,X
+        sta.l $7F0000,X
+        lda.l $7F0010,X
+        eor.w $0010,Y
+        and.w $0020,Y
+        eor.l $7F0010,X
+        sta.l $7F0010,X
+        ; inc
+        inx
+        inx
+        iny
+        iny
+    .ENDR
+    ; while (--count)
+    txa
+    clc
     adc #16
+    tax
+    tya
+    clc
+    adc #48 - 16
     tay
     dec.b $00
     bnel @loop
